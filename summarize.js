248b56a5985ea0140e32804e3bfd200c43a54f31
AJ-O <ashishleiot@gmail.com>
2019-12-03 17:15:32 +0530

    Finally works

---
+++ summarize.js
@@ -0,0 +1,32 @@
+let but = document.getElementById("summary");
+
+but.addEventListener('click', test);
+
+function test(){
+
+    let num = document.getElementById("numLines").value;
+    console.log(num);
+
+    browser.tabs.query({currentWindow: true, active: true}, function(tabs){
+
+
+    let url = tabs[0].url;
+    console.log(url);
+    var data = "key=9d04304f130505f4fa89b9c9d510e397&url=" + url + "&sentences=" + num;
+    console.log(data);
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === this.DONE) {
+        let val = JSON.parse(this.responseText);
+        let element = document.createElement('p')
+        element.innerHTML = val["summary"];
+        document.body.appendChild(element);
+      }
+    });
+
+    xhr.open("POST", "https://api.meaningcloud.com/summarization-1.0");
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xhr.send(data);
+});
+}
